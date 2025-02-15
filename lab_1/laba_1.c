371f659541bce2310eb02d12a665243a8b3d1f56
B1TO43K <misha11092006@gmail.com>
2025-02-15 15:04:12 +1000

    Add files via upload

--- lab_1/laba_1.c
+++ lab_1/laba_1.c
@@ -11,18 +11,26 @@
 int main(int argc, char *argv[]) {
 	double a, b, c;
 	double x1, x2, d;
-	readln(a);
-	b = readln();
-	c = readln();
+	a = 1.2;
+	b = 0.5;
+	c = 3.3;
+	
+	printf("vvedite a: \n");
+	scanf("%lf", &a);
+	printf("vvedite b: \n");
+	scanf("%lf", &b);
+	printf("vvedite c: \n");
+	scanf("%lf", &c);
 	
 	d = pow(b, 2) - 4 * a * c;
-	x1 = -b + sqrt(d)/2*a;
-	x1 = -b - sqrt(d)/2*a;
 	if (d > 0){
+		x1 = -b + sqrt(d)/2*a;
+		x1 = -b - sqrt(d)/2*a;
 		printf("x1 - %lf \n", x1);
 		printf("x2 - %lf \n", x2);	
 	}
 	if (d == 0){
+		x1 = -b + sqrt(d)/2*a;
 		printf("x1 - %lf \n", x1);
 	}
 	if (d < 0){
