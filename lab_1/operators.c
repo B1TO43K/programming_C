8aa0a3cbea1932bed7a5b92c1a79ad269b6cd3dc
B1TO43K <misha11092006@gmail.com>
2025-02-18 16:30:20 +1000

    Add files via upload

---
+++ lab_1/operators.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int sum, raz, proi, del, ost_del, plus1, minus1;
+	int a, b;
+	printf("Vvedite a \n");
+	scanf("%d", &a);
+	printf("Vvedite b \n");
+	scanf("%d", &b);
+	
+	sum = a + b;
+	
+	printf("a + b = %d\n",sum);
+	
+	raz = a - b;
+	
+	printf("a - b = %d\n",raz);
+	
+	proi = a * b;
+	
+	printf("a * b = %d\n",proi);
+	
+	ost_del = a % b;
+	
+	printf("a // b = %d ",ost_del);
+	return 0;
+}
