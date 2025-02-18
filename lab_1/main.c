8e576cdaac842ae81a404cac4e69ae8e3e3df218
B1TO43K <misha11092006@gmail.com>
2025-02-18 15:57:56 +1000

    Add files via upload

---
+++ lab_1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main(){
+int age = 25;
+char hello[] = "privet mir!";
+	printf("%s\n", hello);
+	printf("%d\n", age);
+	
+	return 0;
+}
