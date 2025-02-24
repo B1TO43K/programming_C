45a2b4369738d1f880972b777a8380a3fffedcfe
B1TO43K <misha11092006@gmail.com>
2025-02-24 22:43:50 +1000

    Add files via upload

--- laba_2/main.c
+++ laba_2/main.c
@@ -11,7 +11,7 @@
 
 int main(int argc, char *argv[]) {
 	double mat[3][3], sumG, sumP;
-	int i, j;
+	int i, j, p, matr[2][2];
 	printf("Vvedite 1i element matrici\n");
 	scanf("%lf", &mat[0][0]);
 	
@@ -53,5 +53,24 @@ int main(int argc, char *argv[]) {
 	
 	sumP = mat[0][2] + mat[1][1] + mat[2][0];
 	printf("\nSumma pobochnoi diagonali = %.2lf", sumP);
+	
+
+	printf("\nVvedite element[0][0] matrici\n");
+	scanf("%d", &matr[0][0]);
+	printf("Vvedite  element[0][1] matrici\n");
+	scanf("%d", &matr[0][1]);
+	printf("Vvedite  element[1][0] matrici\n");
+	scanf("%d", &matr[1][0]);
+	printf("Vvedite  element[1][1] matrici\n");
+	scanf("%d", &matr[1][1]);
+	
+	printf("Kvadrat vashei matrici imeet sleduihii vid:\n");
+	for (i = 0; i < 2; i++){
+		for (j = 0; j < 2; j++){
+			p = matr[i][j] * matr[i][j];
+			printf("%d ", p);
+        }
+    	printf("\n");
+    }
 	return 0;
 }
