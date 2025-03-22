049639dea59f66afec72c3195a84da8d0f26c893
B1TO43K <misha11092006@gmail.com>
2025-03-22 13:56:41 +1000

    Update main.c

--- laba_2/main.c
+++ laba_2/main.c
@@ -3,17 +3,22 @@
 
 int main() {
 
-  double a[3][3], sum1 = 0, sum2 = 0;
+  double sum1 = 0, sum2 = 0;
   int i, j, n, N, k;
 
-  printf("Enter 9 numbers for the array double:\n"); 
-  for (i = 0; i < 3; i++)                     
-    for (j = 0; j < 3; j++)                    
+	
+printf("Enter n (n * n): ");  	
+scanf("%d", &n);
+double a[n][n]; 
+  N = pow(n, 2); 
+  printf("Enter %d numbers for the array int:\n", N); 
+  for (i = 0; i < n; i++)                     
+    for (j = 0; j < n; j++)                    
       scanf("%lf", &a[i][j]);                 
 
-  for (i = 0; i < 3; i++) {               
+  for (i = 0; i < n; i++) {               
     sum1 += a[i][i];                           
-    sum2 += a[i][2 - i];                          
+    sum2 += a[i][(n-1) - i];                          
   }
 
   printf("The sum of the main diagonal: %.2lf\n", sum1);   
